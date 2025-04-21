bf6bf49b457ee9d9a548f13a12d574a44c4d425c
Kerly Sarrias <kerlysarrias011@gmail.com>
2025-04-21 18:28:22 -0500

    updated routes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-fetch('/pages/header.html')
-.then(res => res.text())
-.then(data => document.getElementById('header').innerHTML = data);
+const basePath = location.pathname.includes('/Pages/') ? '../components/' : 'components/';
 
-fetch('/pages/footer.html')
-.then(res => res.text())
-.then(data => document.getElementById('footer').innerHTML = data);
+fetch(`${basePath}header.html`)
+  .then(res => res.text())
+  .then(data => document.getElementById('header').innerHTML = data);
+
+fetch(`${basePath}footer.html`)
+  .then(res => res.text())
+  .then(data => document.getElementById('footer').innerHTML = data);
